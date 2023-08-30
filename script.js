89753646e032fc37e07988f9f631e114da5b4d32
Francesca Marshall <francescam3105@gmail.com>
2023-08-31 00:12:49 +0100

    added form

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const fetchCountries = () => { 
-    const request = fetch("https://restcountries.com/v3.1/all");
-    console.log(request);
+    const request = fetch("https://restcountries.com/v3.1/all")
+    .then(response => response.json())
+    .then(response => console.log(response))
 }
 
 fetchCountries();
