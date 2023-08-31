d5b78a199a413fc6926735c9f23b9e135cac0a1e
Francesca Marshall <francescam3105@gmail.com>
2023-08-31 02:04:26 +0100

    added filter

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // global variable 
 let countries = [];
 
-const fetchCountries = async () => { 
-    const reponse = await fetch("https://restcountries.com/v3.1/all")
-    .then(response => response.json())
-    return reponse;
+async function fetchCountries() { 
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    const data = await response.json();
+    countries = data;
+    return data;
 }
 
 async function setUp() {
@@ -18,19 +19,30 @@ function addToCountries(data) {
     ul.innerHTML = '';
     data.forEach((country) => {
         const li = document.createElement('li');
-        li.textContent(`Name ${country.name.common}, Population ${country.population}`);
+        li.textContent = `Name ${country.name.common}, Population ${country.population}`;
         ul.appendChild(li);
-    })
+    });
 }
 
 
 function handleForm(event) {
     event.preventDefault();
-    const input = document.querySelector('input[type="text"]');
-   console.log('Input value:', input.value);
+    const input = document.querySelector("input[type='text']");
+    console.log("Input value:", input.value);
+    filterCountries(input.value);
 }
 
-setUp
+function filterCountries(keyword) {
+    const filteredCountries = countries.filter(country =>
+      country.name.common.toLowerCase().includes(keyword.toLowerCase())
+    );
+    addToCountries(filteredCountries);
+  }
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleForm);
+
+window.addEventListener('load', setUp);
 
 
 // document.querySelector("button").addEventListener("click", fetchCountries());
