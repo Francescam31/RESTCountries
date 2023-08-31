9489866817ca76a3b35df29dce82b8d3176f5a8e
Francesca Marshall <francescam3105@gmail.com>
2023-08-31 01:24:31 +0100

    added function to create list in global variable

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ const fetchCountries = async () => {
 function setUp() {
     const data = fetchCountries();
     // add countries to global variable
+    addToCountries(data);
+}
+
+function addToCountries() {
+    const ul = document.querySelector('ul');
+    data.forEach(country => {
+        const li = document.createElement('li');
+        li.textContent(`Name ${country.name.common}, Population ${country.population}`);
+        ul.appendChild(li);
+    })
 }
 
 
